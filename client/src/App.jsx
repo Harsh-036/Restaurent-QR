e063483f2dbbb1b9eee16e82b567f63ad8c95148
Deepak Vaishnav <deepakvaishnav.dev@gmail.com>
2025-11-25 13:28:28 +0530

    initial change

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-orange-500 text-white'>react</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  passwordHash: {
+    type: String
+  },
+  isActive: {
+    type: Boolean
+  },
+  refreshToken: {
+    type: String
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
