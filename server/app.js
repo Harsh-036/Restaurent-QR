5761d905aeaaef3059c25dbf5f48f187190e66c8
Deepak Vaishnav <deepakvaishnav.dev@gmail.com>
2025-11-25 13:57:35 +0530

    change vercel

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express();
 
+app.get('/', (req,res)=>{
+    res.send('Homepage')
+})
+
 app.listen(3000, ()=>{
     console.log('server is running on 3000')
 }) 
